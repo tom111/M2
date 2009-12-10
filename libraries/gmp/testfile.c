c55710d99b3d0d19cfdb360459e1476469b2cf55
Daniel Grayson <dan@math.uiuc.edu>
2009-12-10 06:13:35 +0000

    update test file to show bug in mpir
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/release-branches/1.3.1@9983 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- libraries/gmp/testfile.c
+++ libraries/gmp/testfile.c
@@ -1,60 +1,54 @@
+/*
+
+   Bug in mpir 1.2.1.
+
+   This program demonstrates that mpn_lshift, as implemented on 64 bit intel machines by mpn/lshift.as, gives
+   the wrong answer if the limbs are not aligned to an 8 byte boundary.
+
+   Even if the default allocator always returns 8-byte aligned memory blocks, this is a bug, because the
+   documentation on "Custom Allocation" doesn't require custom memory allocators to obey any alignment requirements.
+  
+   Here's the output from the program
+
+	5285209847924392297689518137364380393182068736
+	5285209916707841180605126409194605050347913216
+
+   Correct output would have the second line the same as the first.
+
+   Linux fedora64 2.6.31.6-162.fc12.x86_64 #1 SMP Fri Dec 4 00:06:26 EST 2009 x86_64 x86_64 x86_64 GNU/Linux
+
+   gcc version 4.4.2 20091027 (Red Hat 4.4.2-7) (GCC) 
+
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
 #include <assert.h>
 
 int main (int argc, char **argv) {
-  mpz_t dividend, divisor, quotient, remainder, quotient2, remainder2, temp;
-  mpz_init (dividend);
-  mpz_init (divisor);
-  mpz_init (quotient);
-  mpz_init (remainder);
-  mpz_init (quotient2);
-  mpz_init (remainder2);
-  mpz_init (temp);
-  assert(0 == mpz_set_str (dividend, "101506308928235398946510927768391420311764992000", 10));
-  assert(0 == mpz_set_str (divisor, "27656345068767491604576153420888539136", 10));
-  mpz_fdiv_qr (quotient, remainder, dividend, divisor);
-  printf("dividend  = ");
-  mpz_out_str(stdout, 10, dividend);
+  mpz_t rp, sp;
+  mpz_init2 (rp, 10 * 64);
+  mpz_init2 (sp, 10 * 64);
+  mpz_set_str (rp, "660651230990549037211189767170547549147758592", 10);
+
+  mpz_set_str (sp, "660651230990549037211189767170547549147758592", 10);
+  mpn_lshift (rp -> _mp_d, sp -> _mp_d, 3, 3);
+  mpz_out_str(stdout, 10, rp);
   printf("\n");
-  printf("divisor   = ");
-  mpz_out_str(stdout, 10, divisor);
+
+  sp->_mp_d = (mp_limb_t *)((char *)sp->_mp_d + 4);
+
+  mpz_set_str (sp, "660651230990549037211189767170547549147758592", 10);
+  mpn_lshift (rp -> _mp_d, sp -> _mp_d, 3, 3);
+  mpz_out_str(stdout, 10, rp);
   printf("\n");
-  printf("quotient  = ");
-  mpz_out_str(stdout, 10, quotient);
-  printf("\n");
-  printf("remainder = ");
-  mpz_out_str(stdout, 10, remainder);
-  printf("\n");
-  mpz_fdiv_q (quotient2, dividend, divisor);
-  mpz_fdiv_r (remainder2, dividend, divisor);
-  assert(mpz_cmp (quotient, quotient2) == 0);
-  assert(mpz_cmp (remainder, remainder2) == 0);
-  assert(!((mpz_cmp_ui (quotient, 0) != 0) &&
-	   ((mpz_cmp_ui (quotient, 0) < 0)
-	    != ((mpz_cmp_ui (dividend, 0) ^ mpz_cmp_ui (divisor, 0)) < 0))));
-  assert(!((mpz_cmp_ui (remainder, 0) != 0) &&
-	   ((mpz_cmp_ui (remainder, 0) < 0) != (mpz_cmp_ui (divisor, 0) < 0))));
-  mpz_mul (temp, quotient, divisor);
-  mpz_add (temp, temp, remainder);
-  assert(mpz_cmp (temp, dividend) == 0);
-  mpz_abs (temp, divisor);
-  mpz_abs (remainder, remainder);
-  assert(mpz_cmp (remainder, temp) < 0);
-  mpz_clear (dividend);
-  mpz_clear (divisor);
-  mpz_clear (quotient);
-  mpz_clear (remainder);
-  mpz_clear (quotient2);
-  mpz_clear (remainder2);
-  mpz_clear (temp);
-  printf("test passed\n");
+
   return 0;
 }
 
 /*
  Local Variables:
- compile-command: "make -C $M2BUILDDIR/libraries/gmp testfile LOADLIBES=-lgmp LDFLAGS=-L../final/lib CPPFLAG=-I../final/include && (set -x ; $M2BUILDDIR/libraries/gmp/testfile )"
+ compile-command: "make -C $M2BUILDDIR/libraries/gmp testfile LOADLIBES=-lgmp LDFLAGS='-Wl,-Map,mapfile -L../final/lib' CPPFLAG=-I../final/include && (set -x ; grep LOAD $M2BUILDDIR/libraries/gmp/mapfile ; $M2BUILDDIR/libraries/gmp/testfile )"
  End:
 */
